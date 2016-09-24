749da29eda382dfdda67143e4f71737fa543fdeb
muriloman <manzolimurilo@gmail.com>
2016-09-24 16:27:17 -0300

    Added contatos page and routes

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
 //public/js/main.js
 
 //angular é um objeto global, acessa varios recursos do framework
-angular.module("contatooh", ["ng-route"]);
+angular.module("contatooh", ["ng-route"])
+	   .config(function ($routeProvider) {
+		$routeProvider.when("/contatos", {
+		  templateUrl: "partials/contatos.html",
+		  controller: "ContatosController"
+		});
+	   });
