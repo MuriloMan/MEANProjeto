378831a8c493f74d947dfcd325b739e7b06aefcb
muriloman <manzolimurilo@gmail.com>
2016-09-24 16:50:11 -0300

    Added contato route

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,10 +1,9 @@
 //Public/js/controllers/ContatosController.js
-angular.module("contatooh").controller("ContatosController",
-	   function ($scope) {
-		
-		$scope.total = 0;
-		
-		$scope.incrementa = function(){
-		  $scope.total++;
-		}
-	   });
+angular.module("contatooh").controller("ContatosController", function ($scope, $routeParams) {
+  console.log($routeParams.contatoId);
+  $scope.total = 0;
+
+  $scope.incrementa = function () {
+    $scope.total++;
+  }
+});
