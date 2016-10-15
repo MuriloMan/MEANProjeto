ab8ae18efd60a20aacd8c758098e737fa50d8681
muriloman <manzolimurilo@gmail.com>
2016-10-15 15:26:26 -0300

    salvando contato novo

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,17 +1,26 @@
 angular.module('contatooh').controller('ContatoController',
 	   function ($scope, $routeParams, $resource) {
-		
-		if($routeParams.contatoId){
-		var Contato = $resource('/contatos/:id');//:id tem q ser igual ao
-		Contato.get({id: $routeParams.contatoId},//:id dessa linha
-			   function (contato) {
-				$scope.contato = contato;
-			   }, function (erro) {
-		  $scope.mensagem = {
-		    texto: 'Erro ao obter contato'
-		  };
-		});
-	   }else{
-		$scope.contato = {};
-	   }
+		$scope.salva = salva;
+		function salva() {
+		  $scope.contato.$save()
+				.then(function () {
+				  $scope.mensagem = {texto: 'salvo com sucesso'};
+				  $scope.contato = new Contato();
+				}).catch(function () {
+		    $scope.mensagem = {texto: 'Falha ao salvar'};
+		  });
+		}
+		if ($routeParams.contatoId) {
+		  var Contato = $resource('/contatos/:id');//:id tem q ser igual ao
+		  Contato.get({id: $routeParams.contatoId}, //:id dessa linha
+				function (contato) {
+				  $scope.contato = contato;
+				}, function (erro) {
+		    $scope.mensagem = {
+			 texto: 'Erro ao obter contato'
+		    };
+		  });
+		} else {
+		  $scope.contato = {};
+		}
 	   });
