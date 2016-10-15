b9e08463a2be02b22f4668b0fc1687019cfb0ba5
muriloman <manzolimurilo@gmail.com>
2016-10-15 14:10:16 -0300

    criado form contato.html

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -5,6 +5,7 @@ angular.module("contatooh").controller("ContatosController", function ($scope, $
   };
   $scope.contatos = [];
   $scope.filtro = "";
+  $scope.mensagem = {texto: ''};
 
   var Contato = $resource("/contatos/:id");
 
@@ -12,16 +13,24 @@ angular.module("contatooh").controller("ContatosController", function ($scope, $
     Contato.query(
 		  function (contatos) {
 		    $scope.contatos = contatos;
+		    $scope.mensagem = {texto:''};
 		  }, function (erro) {
+	 $scope.mensagem = {
+	   texto: 'Não foi possivel obter a lista'
+	 };
 	 console.log("Erro: " + erro);
     });
+
   }
   buscaContatos();
   $scope.remove = function (contato) {
     Contato.delete({id: contato._id},
-		  buscaContatos,//success 2nd function
+		  buscaContatos, //success 2nd function
 		  function (erro) { //fail 3rd function
 		    console.log("Erro ao recarregar lista de contatos: " + erro);
+		    $scope.mensagem = {
+			 texto: 'Não foi possivel remoder da lista'
+		    };
 		  });
   };
 });
